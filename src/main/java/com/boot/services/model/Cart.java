4349112a5b123ef409fed9caf347830f8a520e4c
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-04-12 09:40:02 +0300

    added @PreUpdate on lastUpdatedOn field

--- src/main/java/com/boot/services/model/Cart.java
+++ src/main/java/com/boot/services/model/Cart.java
@@ -4,18 +4,7 @@ package com.boot.services.model;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -44,4 +33,8 @@ public class Cart {
 	@JoinColumn(name = "user_id", referencedColumnName = "id")
 	private User user;
 
+	@PreUpdate
+	protected void lastUpdatedOnPreUpdate() {
+		this.lastUpdatedOn =  LocalDateTime.now();
+	}
 }
